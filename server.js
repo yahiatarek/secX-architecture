eb083fda110617ad1429dac0681c1b0611dd1482
yehia <yahia.yt@gmail.com>
2023-05-19 02:35:11 +0300

    reading files

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -8,9 +21,25 @@ app.set('view engine', 'ejs');
 // Set the path for views
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Define a route
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/uploadFile', upload.single('file'), (req, res) => {
+  // Get the path of the uploaded file
+  const fileRead = req.file.path;
+
+  // Read the contents of the file
+  fs.readFile(fileRead, 'utf8', (err, data) => {
+    try {
+      res.render('index');
+    } catch (err) {
+      console.log(err)
+    }
+  });
 });
 
 // Start the server
